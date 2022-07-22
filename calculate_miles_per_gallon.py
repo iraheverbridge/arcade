126f25b37c9653ee5e5ed6440dc645c63e37409d
Irah-Mariey.Sitchon <irah.everbridge@gmail.com>
2022-07-22 11:17:52 +0200

    Day 2

---
+++ calculate_miles_per_gallon.py
@@ -0,0 +1,8 @@
+print("This Program calculate the MPG")
+
+miles_driven = float(input("Enter Miles Driven: "))
+gallon_used = float(input("Enter gallon used: "))
+
+mpg = miles_driven/gallon_used
+
+print("The MPG is ", mpg)
---
+++ worksheets/worksheet01.py
@@ -0,0 +1,5 @@
+import names
+
+randomName = names.get_last_name()
+randomLastName = names.get_last_name()
+print(randomName, randomLastName)
