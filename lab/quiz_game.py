dd2455b2f33a919a76283bc9a36d2584fae603ab
Irah-Mariey.Sitchon <irah.everbridge@gmail.com>
2022-07-28 11:15:59 +0200

    Added quiz.py

--- lab/quiz_game.py
+++ lab/quiz_game.py
@@ -1,18 +1,38 @@
 
 print("This is a simple Quiz Game")
 
-answer = 2
-
-print(input("1. What is 1 + 1: "))
+answer = float(input("1. What is 1 + 1: "))
 if answer == 2:
     print("You are correct")
-else answer != 2:
+elif answer != 2:
     print("You are not correct")
 
-print(input("2. What is the combination color of yellow and blue?"))
+answer = input("2. What is the combination color of yellow and blue?: ")
+if answer.lower() == 'green':
+    print("You are correct")
+elif answer.lower() != 'green':
+    print("You are not correct")
 
-print(input("3. What is the number next to 7?"))
+answer = (input("3. What is the number next to 7?: "))
+if answer == '8':
+    print("You are correct")
+elif answer != '8':
+    print("You are not correct")
 
-print(input("4. How many days are there in a week?"))
+answer = int(input("4. How many days are there in a week?: "))
+if answer == 7:
+    print("You are correct")
+elif answer != 7:
+    print("You are not correct")
 
-print(input("5. What are is the color of a sun?"))
+answer = input("5. What are is the color of a sun?: ")
+if answer == 'yellow':
+    print("You are correct")
+elif answer != 'yellow':
+    print("You are not correct")
+
+options = ['You are correct', 'You are not correct']
+
+number_correct = options.count("You are correct")
+
+print("Congrats, You got ", number_correct, "!")
